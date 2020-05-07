4c084d4d5d72db064f005c85d7a50617ef0d622b
Leanne Chan <54755036+LeanneChan@users.noreply.github.com>
2020-05-07 20:58:58 +0800

    Added functionality
    added functionality to side bar, allowed filtering by score as well as by engine, allow to reset map (going back to original position)

--- helperfunctions.js
+++ helperfunctions.js
@@ -1,19 +1,199 @@
 /* ============= Helper Functions ============== */
 // Compile inputs from sidebar into a dictionary.
-var readInput = function() {
-  var inputs = {
-    firerisk : $('#firerisk').change(function() {$(this).val($(this).is(':checked')); }).change().val()=='true',
-    social : $('#social').change(function() { $(this).val($(this).is(':checked')); }).change().val()=='true',
-    hydrant : $('#hydrantquality').change(function() { $(this).val($(this).is(':checked')); }).change().val()=='true',
-    industrial : $("#industrial").change(function() { $(this).val($(this).is(':checked')); }).change().val()=='true',
-    score: $("myRange").slider('option','value')
-  };
-  return inputs;
-};
-
 // When the checkbox for an option is selected, that layer will be shown on the map.
 var addOption = function(checkedTrue, layerName) {
   if(checkedTrue) {
     map.setLayoutProperty(layerName, 'visibility', "visible");
   }
 };
+
+
+var appState = {
+  "fireScore": undefined,
+  "socialScore": undefined,
+  "industrialScore": undefined,
+  "hydrantScore": undefined,
+  "scoreFilter": undefined,
+  "filter_on": undefined,
+  "eng_filter_on": undefined,
+  "engineFilter": undefined
+}; // note: appState values are only updated when the 'Update Map' button is Clicked! 
+
+/* ============= For reading and updating colour of map ============== */
+
+// read input function to read the new input when button is clicked 
+var readInput = function(){
+  appState.fireScore = $('#cbox-input1')[0].checked;
+  console.log("Include Fire Risk?", appState.fireScore);
+  appState.industrialScore= $('#cbox-input2')[0].checked;
+  console.log("Include Percent Industrial", appState.industrialScore);
+  appState.socialScore = $('#cbox-input3')[0].checked;
+  console.log("Include Social Impact?", appState.socialScore);
+  appState.hydrantScore = $('#cbox-input4')[0].checked;
+  console.log("Include Hydrant Age?", appState.hydrantScore);
+  appState.filter_on = $('#cbox-input5')[0].checked;
+  console.log("filter scores on: ", appState.filter_on);
+  appState.scoreFilter = slider.value;
+  console.log("filter scores by: ", appState.scoreFilter);
+  appState.eng_filter_on = $('#cbox-input6')[0].checked;
+  console.log("engine scores on: ", appState.eng_filter_on);
+  appState.engineFilter = $('#text-input1').val();
+  console.log("Filter to this engine number", appState.engineFilter);
+};
+
+var inputToNum = function(input){
+  if(input){
+    return 1;
+  }else{
+    return 0;
+  }
+}
+
+var inputToCombi = function(){
+  var fir = inputToNum(appState.fireScore);
+  var soc = inputToNum(appState.socialScore);
+  var ind = inputToNum(appState.industrialScore);
+  var age = inputToNum(appState.hydrantScore);
+  console.log("Combination to get from file:" +fir+ind+soc+age)
+  return ""+fir+ind+soc+age
+}
+
+/* ============= Filters  ============== */
+var filterByScores = function(c,s){
+  map.setFilter('hydrants', ['==', ['get', c], s]);
+}
+
+var filterByEngine = function(eng){
+   map.setFilter('hydrants', ['==', ['get', 'ENGINE_NUM'], eng]);
+}
+
+
+/* ============= Update map ============== */
+
+var updateMap = function(combination){
+  // if the hydrants are plotted, remove theme
+  if (combination=="0000"){
+    resetColours();
+  }else{
+    if (map.getLayer('hydrants')){ map.removeLayer('hydrants')};
+    // add new layer 
+    map.addLayer({
+                "id":"hydrants",
+                "type":"circle",
+                'source': 'hydrants',
+                // 'source-layer':'fishJan-bhb97l',
+                'layout': {
+                  'visibility': 'visible'},
+                paint: {
+                 // color circles by year_built_copy, using a match expression
+                 "circle-color": [
+                    'match',
+                    ['get', combination],
+                    1,
+                    '#ffffcc',
+                    2,
+                    '#ffeda0',
+                    3,
+                    '#fed976',
+                    4,
+                    '#feb24c',
+                    5,
+                    '#fd8d3c',
+                    6,
+                    '#fc4e2a',
+                    7,
+                    '#e31a1c', 
+                    8,
+                    '#bd0026',
+                    9,
+                    '#800026',
+                    10,
+                    '#420D09',
+                    '#ccc' //for other that may not be specified
+                  ],
+                 "circle-radius": 2,
+                 "circle-stroke-width": 0.6,
+                 "circle-stroke-color": "#fff",
+                 "circle-opacity":0.3
+                }
+    });
+  }
+}
+
+/* ============= Reset Map ============== */
+var resetColours = function(){
+  // if the hydrants are plotted, remove them 
+  if (map.getLayer('hydrants')){ map.removeLayer('hydrants')};
+  map.addLayer({
+              "id":"hydrants",
+              "type":"circle",
+              'source': 'hydrants',
+              // 'source-layer':'fishJan-bhb97l',
+              'layout': {
+                'visibility': 'visible'},
+              paint: {
+               // color circles by year_built_copy, using a match expression
+               "circle-color":"#756bb1",
+               "circle-radius": 2,
+               "circle-stroke-width": 0.6,
+               "circle-stroke-color": "#fff",
+               "circle-opacity":0.3
+              }
+  });
+
+}
+
+var resetValues = function(){
+  $("#cbox-input1").prop("checked", false);
+  $("#cbox-input2").prop("checked", false);
+  $("#cbox-input3").prop("checked", false);
+  $("#cbox-input4").prop("checked", false);
+  $('#cbox-input5').prop("checked", false);
+  $('#cbox-input6').prop("checked", false);
+  $('#text-input1').val('');
+
+  // Push Slider back to 1
+  slider.value=1;
+  output.innerText = 1;
+  map.flyTo({
+    center: [-75.155802, 39.995313],
+    zoom: 10,
+    essential: true // this animation is considered essential with respect to prefers-reduced-motion
+  });
+}
+
+
+/* ============= Button Clicks ============== */
+
+var buttonClick = function(){
+  $('#plotbutton').click(function(e) {
+    readInput(); //updates appstate
+    var c =inputToCombi();
+    updateMap(c);  
+    if (appState.filter_on){
+      //filter by score 
+      var s = Number(appState.scoreFilter);
+      filterByScores(c,s);
+    };
+    if (appState.eng_filter_on){
+      var engine = Number(appState.engineFilter)
+      filterByEngine(engine)
+    }
+  });
+
+  $('#resetbutton').click(function(e) {
+    // clear colours on map
+    resetColours();
+    // reset the toggles
+    resetValues();
+  });
+}
+
+/* ============= Executing these functions ============== */
+$(document).ready(function() {
+  buttonClick();  
+});
+
+
+
+
--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 /* ============= Mapbox setup ============== */
 mapboxgl.accessToken = 'pk.eyJ1Ijoibmp4aW5yYW4iLCJhIjoiY2s4dWxxaHR6MGNobDNtcDZzY2l2OXlyaCJ9.9rS7yysTlpkr_ZeB4vX4Ng';
-var map = new mapboxgl.Map({
-container: 'map',
-style: 'mapbox://styles/mapbox/light-v10',
-center: [-75.165222, 39.952583],
-zoom: 10
+map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/light-v10',
+  center: [-75.155802, 39.995313],
+  zoom: 10
 });
 
+
+/* ============= DATA SET UP ============== */
 var url1= "https://raw.githubusercontent.com/liziqun/MUSA_800/master/deciles_by_ENGINE_4326.geojson";
 var url2= "https://raw.githubusercontent.com/liziqun/MUSA800_App/master/data/engine.geojson";
 
@@ -24,98 +26,116 @@ var engines;
       console.log(engines);
     });
 
+/* ============= Legend setup ============== */
+var layers = ['Top Priority', '2', '3', '4', '5', '6', '7', '8', '9', 'Lowest Priority'];
+var colors = ['#ffffcc','#FFEDA0', '#FED976', '#FEB24C', '#FD8D3C', '#FC4E2A', '#E31A1C', '#BD0026', '#800026',  '#420D09'];
+             
+/* ============= MAPS============== */
 map.on('load', function() {
-          //var url1= "https://raw.githubusercontent.com/liziqun/MUSA_800/master/deciles_by_ENGINE_4326.geojson";
-          //var url2= "https://raw.githubusercontent.com/liziqun/MUSA800_App/master/data/engine.geojson";
-map.addSource('data', {
-          type: 'geojson',
-          data: hydrants
-        });
+  for (i = 0; i < layers.length; i++) {
+    var layer = layers[i];
+    var color = colors[i];
+    var item = document.createElement('div');
+    var key = document.createElement('span');
+    key.className = 'legend-key';
+    key.style.backgroundColor = color;
 
-map.addLayer({
-          "id":"data",
-          "type":"circle",
-          'source': 'data',
-          // 'source-layer':'fishJan-bhb97l',
-          'layout': {
-            'visibility': 'visible'},
+    var value = document.createElement('span');
+    value.innerHTML = layer;
+    item.appendChild(key);
+    item.appendChild(value);
+    legend.appendChild(item);
+  }
+  map.addSource('hydrants', {
+            type: 'geojson',
+            data: hydrants
+  });
+
+  // Add hydrant layer 
+  map.addLayer({
+            "id":"hydrants",
+            "type":"circle",
+            'source': 'hydrants',
+            // 'source-layer':'fishJan-bhb97l',
+            'layout': {
+              'visibility': 'visible'},
             paint: {
-             // color circles by year_built_copy, using a match expression
+             // initially just colour all the same (base map)
              "circle-color": "#756bb1",
              "circle-radius": 2,
              "circle-stroke-width": 0.6,
              "circle-stroke-color": "#fff",
              "circle-opacity":0.3
-         }
-          });
+            }
+  });
 
-          map.addSource('engines', {
+  map.addSource('engines', {
                     type: 'geojson',
                     data: engines
-                  });
+  });
 
-          map.addLayer({
-                    "id":"engines",
-                    "type":"fill",
-                    'source': 'engines',
-                    // 'source-layer':'fishJan-bhb97l',
-                    'layout': {
-                      'visibility': 'visible'},
-                      paint: {
-                       // color circles by year_built_copy, using a match expression
-                       'fill-color': '#888888',
-                       'fill-opacity': 0.4,
-                       'fill-outline-color': 'black'
-                   },
-                   'filter': ['==', '$type', 'Polygon']
-                    });
+  // map.addLayer({
+  //               "id":"engines",
+  //               "type":"fill",
+  //               'source': 'engines',
+  //               // 'source-layer':'fishJan-bhb97l',
+  //               'layout': {
+  //                 'visibility': 'visible'},
+  //                 paint: {
+  //                  // color circles by year_built_copy, using a match expression
+  //                  'fill-color': '#888888',
+  //                  'fill-opacity': 0.4,
+  //                  'fill-outline-color': 'black'
+  //              },
+  //              'filter': ['==', '$type', 'Polygon']
+  //  });
 
 });
 
+/* ============= Pop up for each hydrant  ============== */
 // inspect a unit (point) on click
     // When a click event occurs on a feature in the unclustered-point layer, open a popup at the
     // location of the feature, with description HTML from its properties.
-    map.on('click', 'data', function (e) {
-           map.flyTo({ center: e.features[0].geometry.coordinates});
-
-           var coordinates = e.features[0].geometry.coordinates.slice();
-           var description = "<b>Hydrant ID:</b> " + e.features[0].properties.HYDRANTNUM;
-           description += "<br><b>Engine number:</b> " + e.features[0].properties.ENGINE_NUM;
-           description += "<br><b>Year installed:</b> " + e.features[0].properties.YEAR_INSTA;
-           description += "<br><b>Date of last inspection:</b> " + e.features[0].properties.DATEOFLAST;
+map.on('click', 'hydrants', function (e) {
+  map.flyTo({ center: e.features[0].geometry.coordinates});
+  var coordinates = e.features[0].geometry.coordinates.slice(); 
+  var description = "<b>Hydrant ID:</b> " + e.features[0].properties.HYDRANTNUM;
+  description += "<br><b>Engine number:</b> " + e.features[0].properties.ENGINE_NUM;
+  description += "<br><b>Year installed:</b> " + e.features[0].properties.YEAR_INSTA;
+  description += "<br><b>Date of last inspection:</b> " + e.features[0].properties.DATEOFLAST;
 
            //add other elements/ fix into scrollable menu
 
            // Ensure that if the map is zoomed out such that multiple copies of the feature are visible,
            // the popup appears over the copy being pointed to.
-           while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-               coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-           }
+  while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+       coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+   }
 
-           new mapboxgl.Popup()
-               .setLngLat(coordinates)
-               .setHTML(description)
-               .addTo(map);
-    });
+  new mapboxgl.Popup()
+     .setLngLat(coordinates)
+     .setHTML(description)
+     .addTo(map);
+});
 
-    map.on('mouseenter', 'data', function () {
-        map.getCanvas().style.cursor = 'pointer';
-    });
-    map.on('mouseleave', 'data', function () {
-        map.getCanvas().style.cursor = '';
-    });
+map.on('mouseenter', 'hydrants', function () {
+    map.getCanvas().style.cursor = 'pointer';
+});
+map.on('mouseleave', 'hydrants', function () {
+    map.getCanvas().style.cursor = '';
+});
 
-  //var myFeatures = map.querySourceFeatures('engines');
-  //console.log(myFeatures);
+//var myFeatures = map.querySourceFeatures('engines');
+//console.log(myFeatures);
 
-    /* ============= User Interactivity ============== */
-    /* ============= Interactive Elements setup ============== */
+
+/* ============= Show the slider scores ============== */
 var slider = document.getElementById("myRange");
 var output = document.getElementById("demo");
 output.innerHTML = slider.value;
-
 slider.oninput = function() {
   output.innerHTML = this.value;
 };
 
-  /* ============= Interactive Elements setup ============== */
+
+
