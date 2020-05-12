985ef146b85115fbc0cdd5adf400425f0456ae5b
njxinran95 <njxinran@hotmail.com>
2020-05-12 05:14:28 -0400

    Update

--- main.js
+++ main.js
@@ -293,3 +293,22 @@ slider.oninput = function() {
 
 /* ============= Add navigation control bar============== */
 map.addControl(new mapboxgl.NavigationControl());
+
+/* ============= Add modal for showing engines============== */
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("engineBtn");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = 'https://raw.githubusercontent.com/njxinran95/PhillyFire_App/master/EngineMap.jpg';
+  captionText.innerHTML = 'Fire engines with engine numbers';
+};
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+};
